8c558f3fc2fd1c42425fed6ef1bcf38893f19fa6
Srulgerl <saruulgerel.o325@gmail.com>
2025-02-21 23:27:25 -0800

    test dutuu

---
+++ CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorLibrary;
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_ReturnsCorrectSum()
+        {
+            var calculator = new Calculator();
+            calculator.Add(10, 5);
+            Assert.AreEqual(15, calculator.Result);
+
+        }
+
+        [TestMethod]
+        public void Subtract_ReturnsCorrectSum()
+        {
+            var calculator1 = new Calculator();
+            calculator1.Subtract(10, 5);
+            Assert.AreEqual(5, calculator1.Result);
+        }
+    } 
+}
