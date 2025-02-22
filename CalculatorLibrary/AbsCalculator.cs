fe8d38135578b4308b632bd3723631fbc1df4848
Srulgerl <saruulgerel.o325@gmail.com>
2025-02-21 22:33:06 -0800

    /

--- CalculatorLibrary/AbsCalculator.cs
+++ CalculatorLibrary/AbsCalculator.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CalculatorLibrary;
-
+/// <summary>
+/// Тооны машины үндсэн хийсвэр класс.
+/// </summary>
 public abstract class AbsCalculator
 {
+    /// <summary>
+    /// Тооны машины хамгийн сүүлийн тооцооллын үр дүн
+    /// </summary>
     public decimal Result { get; protected set; }
 }
