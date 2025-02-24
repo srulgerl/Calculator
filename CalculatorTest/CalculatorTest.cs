b3d8a3e215e39d89c0d1870feef6519c84652060
Srulgerl <saruulgerel.o325@gmail.com>
2025-02-24 00:57:18 -0800

    test

--- CalculatorTest/CalculatorTest.cs
+++ CalculatorTest/CalculatorTest.cs
@@ -20,5 +20,35 @@ namespace CalculatorTest
             calculator1.Subtract(10, 5);
             Assert.AreEqual(5, calculator1.Result);
         }
-    } 
+
+        [TestMethod]
+        public void Store_ValueInMemory()
+        {
+            var calculator = new Calculator();
+            Memory memory = new Memory();
+            calculator.Add(10, 5);
+            memory.Store(calculator.Result);
+            Assert.AreEqual(15, calculator.Result);
+        }
+        public void Clear_AllMemoryItems()
+        {
+            var memory = new Memory();
+            memory.Store(10);
+            memory.Store(5);
+            memory.Clear();
+            Assert.AreEqual(0, memory.GetHistory().Count);
+        }
+
+        public void Get_History()
+        {
+            var memory = new Memory();
+            memory.Store(10);
+            memory.Store(5);
+            memory.Clear();
+            memory.Store(6);
+            Assert.AreEqual((memory.GetHistory()).Count(), 1);
+            Assert.Equals(6, memory.GetHistory()[0].Value);
+        }
+
+    }
 }
